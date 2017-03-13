a9d35ed35e06dde48e0508b0c731cc90f79e0389
Mathieu Débit <mathieu.debit@gmail.com>
2017-03-13 17:03:30 +0100

    iOS app

---
+++ ios_project/HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by DEBIT Mathieu on 13/03/2017.
+//  Copyright © 2017 DEBIT Mathieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios_project/HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by DEBIT Mathieu on 13/03/2017.
+//  Copyright © 2017 DEBIT Mathieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
